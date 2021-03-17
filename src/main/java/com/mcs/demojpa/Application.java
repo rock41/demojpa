5c3059efacad871a473e18660ed6ac3c032c792b
rock41 <sting41@gmail.com>
2021-03-18 01:01:43 +0900

    Basic Repository 수정

--- src/main/java/com/mcs/demojpa/Application.java
+++ src/main/java/com/mcs/demojpa/Application.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // Post fix 설정을 통해 Impl이 아닌 다른 단어로 대체 가능
 @SpringBootApplication
-@EnableJpaRepositories(repositoryImplementationPostfix = "Default")
+@EnableJpaRepositories(repositoryBaseClass = SimpleMyRepository.class)
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/mcs/demojpa/post/PostRepository.java
+++ src/main/java/com/mcs/demojpa/post/PostRepository.java
@@ -1,6 +1,7 @@
 package com.mcs.demojpa.post;
 
+import com.mcs.demojpa.MyRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository<Post> {
+public interface PostRepository extends MyRepository<Post, Long> {
 }
